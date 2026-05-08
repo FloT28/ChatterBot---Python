4b062cd67cc81922446036ee76267756df3b7c1e
Florence T <florence.taele@gmail.com>
2026-05-08 12:47:20 +1200

    new changes - database and added code

--- bot.py
+++ bot.py
@@ -1,7 +1,24 @@
 #library for chatterbot 
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+#Loading Structured training data from files 
+from chatterbot.trainers import JsonFileTrainer
 
 chatbot = ChatBot("Chatpot")
+
+trainer = JsonFileTrainer(chatbot)
+trainer.train("./data/conversations.json")
+
+#Training for chatbot
+trainer = ListTrainer(chatbot)
+trainer.train([
+    "Hi",
+    "Welcome, friend!",
+])
+trainer.train([
+    "Are you a plant?",
+    "No, I'm the pot below the plant!"
+])
 
 exit_conditions = (":q", "quit", "exit")
 while True: 
@@ -10,4 +27,3 @@ while True:
         break
     else: 
         print(f"{chatbot.get_response(query)}")
-        
